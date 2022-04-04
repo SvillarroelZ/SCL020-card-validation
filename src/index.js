a6e7aedf6ffb43d938379474b3515ac7c7bd3921
camicamposc <clearca57@gmail.com>
2022-04-04 12:26:25 -0400

    cambios de hoy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,69 @@
 import validator from './validator.js';
 
+// var checkbox=document.querySelector(".terms");
+//  console.log(checkbox)
+//       checkbox.querySelector("change", validaCheckbox, false);
+//       function validaCheckbox(){
+//           var checked=checkbox.checked;
+//           if(!checked){
+//               alert("Por favor, acepte los términos y condiciones");
+//           }
+//       }
+
+function pago(){
+boletaFactura=document.querySelector('input[id="pago"]:checked').value;
+// var boletaFactura=document.getElementById("pago").checked;
+        if(boletaFactura.checked==true){
+        alert("Por favor, marque una opción")
+    }
+}
+
+// Intento 1 despues de la Domi
+// var checkbox=document.getElementById("terms");
+// console.log(checkbox)
+//      checkbox.addEventListener("change", validaCheckbox, false);
+//      function validaCheckbox(){
+//          var checked=checkbox.checked;
+//          if(!checked){
+//              alert("Por favor, acepte los términos y condiciones");
+//          }
+//      }
+
+    //intento con la domi
+    //      if (checked){
+    //          alert("Checkbox está seleccionado");
+    //         break;
+    //         }
+    //      else (!checked)
+    //         alert("Por favor, selecciona los términos y condiciones");
+        
+    //  }
+
+//intento 4
+// var terms=document.getElementById("terms").checked;
+//     if(isChecked){
+//         alert("el checkbox esta selecionado");
+//     }
+//validando checkbox//
+
+//intento 1
+// function terms(){
+// var terms=document.getElementById("terms").checked;
+//     if (terms==true)
+//     alert("seleccionaste una alternativa")
+// } 
+
+//intento 2
+
+// var isChecked=document.getElementById("terms").checked;
+//     if (isChecked){
+//         alert("seleccionaste una alternativa");
+//     }
+//     else (isChecked==false){
+//         alert("Por favor acepte los términos y condiciones");
+
+//     }
+
 //Guardar datos de formulario en botón//
 
 var formulario=document.getElementById("formulario");
@@ -11,9 +75,10 @@ formulario.addEventListener("submit", function(e) {
 
 function guardarNombre(){
     var username=document.getElementById("username").value;
-    // var surname=doctument.getElementByID("surname").value;
+    var surname=document.getElementById("surname").value;
 
-        alert(username);  
+        alert (username);  
+        alert (surname);
     window.location.href="./validacion.html"    
 }
 
