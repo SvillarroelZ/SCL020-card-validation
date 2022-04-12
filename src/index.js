541e65b7c35720e2098cedcddccfb72238b9eecb
SvillarroelZ <sofiavillarroelzamora@gmail.com>
2022-04-12 09:49:36 -0400

    index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-////index.html
+// index.html
+
 const formulario = document.getElementById("formulario");   //porque la llamo en varias funciones, por lo tanto queda como constante
+
 //Guardar nombre y apellido para utilizarlo en validacion.html
+
 function guardarNombre() {
     let username = document.getElementById("username").value;
     let surname = document.getElementById("surname").value;
     alert(username + " " + surname);
 }
 // dar formato al rut con . y -
+
 formulario.rut.addEventListener("blur", function () {
     let numRut = document.getElementById("rut").value;
     // console.log(numRut)
@@ -28,23 +32,25 @@ formulario.rut.addEventListener("blur", function () {
         document.getElementById("rut").value = rutFormato;
     }
 });
-// dar formato al celular
-formulario.title.addEventListener("blur", function (){
-    let numTel = document.getElementById("tel").value;
-    if (numTel.length == 11){
-        let div1 = numTel.slice(0, 3);
-        let div2 = numTel.slice(3, 7);
-        let div3 = numTel.slice(7, 11);
-        let numTel = (div1 + div2 + div3);
-        document.getElementById("tel").value = numTel;
-    }
-})
+// // dar formato al celular
+// formulario.tel.addEventListener("blur", function (){
+//     let numTel = document.getElementById("tel").value;
+//     if (numTel.length == 11){
+//         let div1 = numTel.slice(0, 3);
+//         let div2 = numTel.slice(3, 7);
+//         let div3 = numTel.slice(7, 11);
+//         let numTel = (div1 + div2 + div3);
+//         document.getElementById("tel").value = numTel;
+//     }
+// })
+
 //Evitar que el botón de submit haga la funcion de guardar-enviar, luego guarde nombre y apellido y valide checkbox
 formulario.addEventListener("submit", function (e) {
     e.preventDefault()
     guardarNombre()
     validarCheckbox()
 })
+
 // Validar checkbox y redireccionar página a 2do validacion.html
 function validarCheckbox() {
     var checked = document.getElementById("terms").checked;
