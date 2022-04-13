29a90cf4b6ad02be30da8206c7a3facbd3d1c54e
SvillarroelZ <sofiavillarroelzamora@gmail.com>
2022-04-13 15:55:13 -0400

    logopaw

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 // index.html
 
-const formulario = document.getElementById("formulario");   //porque la llamo en varias funciones, por lo tanto queda como constante
+const formulario = document.getElementById("formulario");   
 
-//Guardar nombre y apellido para utilizarlo en validacion.html
-
+// Guardar nombre y apellido para utilizarlo en validacion.html
 function guardarNombre() {
-    let username = document.getElementById("username").value;
-    let surname = document.getElementById("surname").value;
-    let nombreCompleto = (username + " " + surname);
+    let username=document.getElementById("username").value;
+    let surname=document.getElementById("surname").value;
+    let nombreCompleto=(username+" "+surname);
 
     sessionStorage.setItem("Nombre", nombreCompleto);
 }
-// dar formato al rut con . y -
 
+// Dar formato al rut con . y -
 formulario.rut.addEventListener("blur", function () {
-    let numRut = document.getElementById("rut").value;
+    let numRut=document.getElementById("rut").value;
     // console.log(numRut)
     if (numRut.length == 9) {
         let div1 = numRut.slice(0, 2);
         let div2 = numRut.slice(2, 5);
         let div3 = numRut.slice(5, 8);
         let div4 = numRut.slice(8, 9);
-        let rutFormato = (div1 + "." + div2 + "." + div3 + "-" + div4);
+        let rutFormato=(div1 + "." + div2 + "." + div3 + "-" + div4);
         // console.log("rutFormato", rutFormato)
-        document.getElementById("rut").value = rutFormato;
+        document.getElementById("rut").value=rutFormato;
     }
     else {
         let div1 = numRut.slice(0, 1);
         let div2 = numRut.slice(1, 4);
         let div3 = numRut.slice(4, 7);
         let div4 = numRut.slice(7, 8);
-        let rutFormato = (div1 + "." + div2 + "." + div3 + "-" + div4);
-        document.getElementById("rut").value = rutFormato;
+        let rutFormato=(div1 + "." + div2 + "." + div3 + "-" + div4);
+        document.getElementById("rut").value=rutFormato;
     }
 });
+
  // dar formato al celular -> pendiente de consulta
 //  formulario.tel.addEventListener("blur", function (){
 //      let numTelefono = document.getElementById("tel").value;
@@ -52,7 +52,7 @@ formulario.rut.addEventListener("blur", function () {
 //     //  }
 //  })
 
-//Evitar que el botón de submit haga la funcion de guardar-enviar, luego guarde nombre y apellido y valide checkbox
+// Evitar que el botón de submit haga la funcion de guardar-enviar, luego guarde nombre y apellido y valide checkbox
 formulario.addEventListener("submit", function (e) {
     e.preventDefault()
     guardarNombre()
@@ -61,12 +61,12 @@ formulario.addEventListener("submit", function (e) {
 
 // Validar checkbox y redireccionar página a 2do validacion.html
 function validarCheckbox() {
-    var checked = document.getElementById("terms").checked;
+    var checked=document.getElementById("terms").checked;
     if (checked === false) {
-        document.getElementById("errUnchecked").innerHTML = "Debes aceptar los términos y condiciones antes de proceder al pago";
+        document.getElementById("errUnchecked").innerHTML="Debes aceptar los términos y condiciones antes de proceder al pago";
     }
     else {
-        document.getElementById("errUnchecked").innerHTML = checked;
-        window.location.href = "./validacion.html"
+        document.getElementById("errUnchecked").innerHTML=checked;
+        window.location.href="./validacion.html"
     }
 }
--- src/validator.js
+++ src/validator.js
@@ -1,26 +1,26 @@
 const validator = {
 // No borrar de aquí para arriba
 
-isValid: function (validateCard) { // isValid es un metodo de JS que valida el dato de entrada, segun sus propiedades (pattern, value, minlength, etc) 
-  let arrayValidateCard = []; // Crea un array vacío para guardar los numeros invertidos y parseados
+isValid: function(validateCard){ // isValid es un metodo de JS que valida el dato de entrada, segun sus propiedades (pattern, value, minlength, etc) 
+  let arrayValidateCard=[]; // Crea un array vacío para guardar los numeros invertidos y parseados
 
-  for (let i = validateCard.length - 1; i >= 0; i--) { // for recorre cada dígito dentro del string -> lenght -1 llama al ultimo digito del string, y con i-- lo recorro al reves 
-    let caracter = validateCard.charAt(i); // Se posiciona en el indice (en el primer numero del string ya invertido; y crea un nuevo string)
-    let numero = parseInt(caracter); // Guardamos cada uno de los caracteres "parseados" como variable "numero" (desde la primera posición)
+  for (let i=validateCard.length - 1; i >= 0; i--) { // for recorre cada dígito dentro del string -> lenght -1 llama al ultimo digito del string, y con i-- lo recorro al reves 
+    let caracter=validateCard.charAt(i); // Se posiciona en el indice (en el primer numero del string ya invertido; y crea un nuevo string)
+    let numero=parseInt(caracter); // Guardamos cada uno de los caracteres "parseados" como variable "numero" (desde la primera posición)
     arrayValidateCard.push(numero);// Agrega la variable numero al array que creamos arriba (linea 5) mediante push
   }
   
-  let numeroMultiplicadoYSumado = []; // Crea un nuevo array para guardar los numeros de la multiplicacion y la suma
+  let numeroMultiplicadoYSumado=[]; // Crea un nuevo array para guardar los numeros de la multiplicacion y la suma
   
   arrayValidateCard.forEach(function (numero, posicion) { // Recorro el array con el forEach (posiciona cada elemento del array), poniendo el numero a multiplicar[elemento del array], y la posicion[array]
     
     if ((posicion + 1) % 2 === 0) { // Si la posición del digito es par (0,2,4,etc)
-      let numeroMultiplicado = (numero * 2); // multiplica el digito de la posicion par por 2 y agrega el numero multiplicado a la variable numeroMultiplicado.
+      let numeroMultiplicado=(numero * 2); // multiplica el digito de la posicion par por 2 y agrega el numero multiplicado a la variable numeroMultiplicado.
       
       if (numeroMultiplicado >= 10) { // Si el numero multiplicado es mayor o igual a 10 (hay que sumar ambos digitos)
-        let posicionPrimerDigito = numeroMultiplicado.toString().charAt(0, 1); // Pasa el número a String. Corto el primer digito
-        let posicionSegundoDigito = numeroMultiplicado.toString().charAt(1, 2); // Pasa el número a String. Corto el segundo digito
-        let numeroSumado = parseInt(posicionPrimerDigito) + parseInt(posicionSegundoDigito); // Parsea ambos digitos y los suma en la variable numeroSumado
+        let posicionPrimerDigito=numeroMultiplicado.toString().charAt(0, 1); // Pasa el número a String. Corto el primer digito
+        let posicionSegundoDigito=numeroMultiplicado.toString().charAt(1, 2); // Pasa el número a String. Corto el segundo digito
+        let numeroSumado=parseInt(posicionPrimerDigito) + parseInt(posicionSegundoDigito); // Parsea ambos digitos y los suma en la variable numeroSumado
         //console.log(posicionPrimerDigito);
         //console.log(posicionSegundoDigito);
         //console.log(numeroSumado);
@@ -48,7 +48,7 @@ isValid: function (validateCard) { // isValid es un metodo de JS que valida el d
 //Función para enmascarar el numero de la tarjeta dejando los ultimos 4 dígitos libres
   
   maskify: function(validateCard){
-    var punto= "•"; // Símbolo con el que se ocultan los numeros de la tarjeta.
+    var punto="•"; // Símbolo con el que se ocultan los numeros de la tarjeta.
     var hiddenCard=validateCard.slice(-4) // Se cortan los ultimos 4 digitos de la tarjeta
     if (validateCard.length > 4 ){ //Condición= si el largo de los numeros ingresados es mayor a 4, 
         hiddenCard=punto.repeat(validateCard.length-4) + validateCard.slice(-4); // Repite los numeros de la tarjeta con puntos, menos los ultiomos 4
@@ -57,7 +57,6 @@ isValid: function (validateCard) { // isValid es un metodo de JS que valida el d
     return hiddenCard;
  }
  
-
 // no borrar de aqui para abajo
 };
 
